58375afbcd8ad14440e6de71f3ca3b02372f49dd
Tim Oliver <me@timoliver.com.au>
2020-05-14 14:54:26 +0800

    Added Xcode project

---
+++ iOS/App/BluetoothChat/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  BluetoothChat
+//
+//  Created by Tim Oliver on 14/5/20.
+//  Copyright © 2020 DittoLive Incorporated. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
---
+++ iOS/App/BluetoothChat/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BluetoothChat
+//
+//  Created by Tim Oliver on 14/5/20.
+//  Copyright © 2020 DittoLive Incorporated. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
+
