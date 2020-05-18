268d3cec62991839d4689362423648ceb29e6bb0
Tim Oliver <me@timoliver.com.au>
2020-05-19 00:08:32 +0800

    Refined central manager class

--- iOS/App/BluetoothChat/Services/BluetoothCentralManager.swift
+++ iOS/App/BluetoothChat/Services/BluetoothCentralManager.swift
@@ -13,22 +13,68 @@ import CoreBluetooth
 /// in a connected Bluetooth session
 final class BluetoothCentralManager: NSObject {
 
-    private var centralManager: CBCentralManager!
+    // The Core Bluetooth object that manages our state as a central
+    private var centralManager: CBCentralManager?
 
-    override init() {
-        super.init()
-        centralManager = CBCentralManager(delegate: self, queue: nil)
+    // Whether scanning has been deferred
+    private var scanPending = false
 
-    }
-
+    /// Start scanning for peripherals in the area
     public func start() {
-        centralManager.scanForPeripherals(withServices: [BluetoothService.chatServiceID], options: nil)
+        // Create the central manager (This will also kickstart launching Bluetooth)
+        if centralManager == nil {
+            centralManager = CBCentralManager(delegate: self, queue: nil)
+        }
+
+        // We can't begin scanning until Bluetooth has finished powering on,
+        // which might not be ready yet, so we have to defer
+        guard centralManager?.state == .poweredOn else {
+            scanPending = true
+            return
+        }
+
+        // If Bluetooth was ready, start scanning now
+        startScanning()
     }
 }
 
+// MARK: - Private -
+
+extension BluetoothCentralManager {
+    fileprivate func startScanning() {
+        guard !(centralManager?.isScanning ?? true) else { return }
+
+        centralManager?.scanForPeripherals(withServices: [BluetoothService.chatServiceID], options: nil)
+
+        scanPending = false
+    }
+}
+
+// MARK: - Central Manager Delegate -
+
 extension BluetoothCentralManager: CBCentralManagerDelegate {
     func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        switch central.state {
+        case .unknown:
+            print("Bluetooth state has changed to is .unknown")
+        case .resetting:
+            print("Bluetooth state has changed to is .resetting")
+        case .unsupported:
+            print("Bluetooth state has changed to is .unsupported")
+        case .unauthorized:
+            print("Bluetooth state has changed to is .unauthorized")
+        case .poweredOff:
+            print("Bluetooth state has changed to is .poweredOff")
+        case .poweredOn:
+            print("Bluetooth state has changed to is .poweredOn")
+            if scanPending { startScanning() }
+        @unknown default:
+            print("Bluetooth state has changed to an unknown state")
+        }
+    }
 
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        print("FOUND")
     }
 
 }
