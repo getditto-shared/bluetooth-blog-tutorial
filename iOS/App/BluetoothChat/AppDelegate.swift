c54ed62866ee932f91aebbe477b66e137b3ea6b0
Tim Oliver <me@timoliver.com.au>
2020-05-19 00:08:42 +0800

    Cleaned up syntax warnings

--- iOS/App/BluetoothChat/AppDelegate.swift
+++ iOS/App/BluetoothChat/AppDelegate.swift
@@ -11,8 +11,8 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         return true
     }
 }
-
