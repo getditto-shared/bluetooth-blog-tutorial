9f6b537a30770229092e60133fbf6858668fad71
Tim Oliver <me@timoliver.com.au>
2020-05-20 22:53:29 +0800

    Added delegate callbacks for detecting a device, and then connecting to one

--- iOS/App/BluetoothChat/Services/BluetoothCentralManager.swift
+++ iOS/App/BluetoothChat/Services/BluetoothCentralManager.swift
@@ -19,6 +19,9 @@ final class BluetoothCentralManager: NSObject {
     // Whether scanning has been deferred
     private var scanPending = false
 
+    // The peripheral we've connected to
+    private var peripheral: CBPeripheral?
+
     /// Start scanning for peripherals in the area
     public func start() {
         // Create the central manager (This will also kickstart launching Bluetooth)
@@ -41,6 +44,8 @@ final class BluetoothCentralManager: NSObject {
 // MARK: - Private -
 
 extension BluetoothCentralManager {
+    /// Once Bluetooth is powered up and ready, start scanning for peripherals
+    /// advertising the service we support
     fileprivate func startScanning() {
         guard !(centralManager?.isScanning ?? true) else { return }
 
@@ -48,11 +53,15 @@ extension BluetoothCentralManager {
 
         scanPending = false
     }
+
 }
 
 // MARK: - Central Manager Delegate -
 
 extension BluetoothCentralManager: CBCentralManagerDelegate {
+    /// Called whenever the state of the Bluetooth Manager changes.
+    /// This will primarily be used to detect when Bluetooth has finished powering on
+    /// at the beginning
     func centralManagerDidUpdateState(_ central: CBCentralManager) {
         switch central.state {
         case .unknown:
@@ -73,8 +82,38 @@ extension BluetoothCentralManager: CBCentralManagerDelegate {
         }
     }
 
-    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
-        print("FOUND")
+    /// Called when a potentially compatible device was discovered during a scan
+    func centralManager(_ central: CBCentralManager,
+                        didDiscover peripheral: CBPeripheral,
+                        advertisementData: [String: Any],
+                        rssi RSSI: NSNumber) {
+
+        // Ignore any other peripherals we see in the meanwhile
+        guard self.peripheral == nil else { return }
+
+        //
+        if let deviceName = advertisementData[CBAdvertisementDataLocalNameKey] {
+            print("Peripheral \(deviceName) discovered.")
+        } else {
+            print("Compatible device discovered.")
+        }
+
+        // Print the Relative Signal Strength
+        print("RSSI is \(RSSI)")
+
+        // We *must* retain a strong reference to the peripheral object, otherwise it will be released
+        // while we're connecting to it
+        self.peripheral = peripheral
+
+        // Once we've discovered a peripheral matching our service ID, try connecting to it
+        centralManager?.connect(peripheral, options: nil)
+    }
+
+    /// Called when we've managed to establish a connection to a peripheral
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+
+
+
     }
 
 }
