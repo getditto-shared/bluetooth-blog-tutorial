ce3217bdb3002f1417191276160ac27872723053
Tim Oliver <me@timoliver.com.au>
2020-05-29 15:56:20 +0800

    Added device table view cell class

---
+++ iOS/App/BluetoothChat/Scenes/Join/DeviceTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  DeviceTableViewCell.swift
+//  BluetoothChat
+//
+//  Created by Tim Oliver on 29/5/20.
+//  Copyright © 2020 DittoLive Incorporated. All rights reserved.
+//
+
+import UIKit
+
+class DeviceTableViewCell: UITableViewCell {
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+    
+
+}
