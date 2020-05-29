5718cd478adc69a516ca5d47377093c88555e253
Tim Oliver <me@timoliver.com.au>
2020-05-29 14:41:43 +0800

    Added text field table cell

---
+++ iOS/App/BluetoothChat/Scenes/Join/TextFieldTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TextFieldTableViewCell.swift
+//  BluetoothChat
+//
+//  Created by Tim Oliver on 29/5/20.
+//  Copyright © 2020 DittoLive Incorporated. All rights reserved.
+//
+
+import UIKit
+
+class TextFieldTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
