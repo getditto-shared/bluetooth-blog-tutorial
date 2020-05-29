19fd366ddb24754bb48e1a811ec328b20a7cbd31
Tim Oliver <me@timoliver.com.au>
2020-05-29 15:26:58 +0800

    Added table view logic for the join scene

--- iOS/App/BluetoothChat/Scenes/Join/JoinViewController.swift
+++ iOS/App/BluetoothChat/Scenes/Join/JoinViewController.swift
@@ -8,19 +8,57 @@
 
 import UIKit
 
+struct Sections {
+    static let name = 0
+    static let availableDevices = 1
+}
+
 class JoinViewController: UITableViewController {
+
+    static let deviceCellIdentifier = "DeviceCell"
+    static let nameCellIdentifier = "NameCell"
+
+    // MARK: - Class Creation -
+
+    init() {
+        super.init(style: .insetGrouped)
+        tableView.cellLayoutMarginsFollowReadableWidth = true
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - View Configuration -
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        // Register the cells we plan to use
+        tableView.register(UITableViewCell.self,
+                           forCellReuseIdentifier: JoinViewController.deviceCellIdentifier)
+        tableView.register(UINib(nibName: "TextFieldTableViewCell", bundle: nil),
+                           forCellReuseIdentifier: JoinViewController.nameCellIdentifier)
     }
 
-    // MARK: - Table view data source
+    // MARK: - Table View Data Source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
+        return 2
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        if section == Sections.name { return 1 }
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.section == Sections.name {
+            let cell = tableView.dequeueReusableCell(withIdentifier: JoinViewController.nameCellIdentifier,
+                                                     for: indexPath)
+            return cell
+        }
+
+        return tableView.dequeueReusableCell(withIdentifier: JoinViewController.deviceCellIdentifier, for: indexPath)
     }
 }
--- iOS/App/BluetoothChat/Scenes/Join/TextFieldTableViewCell.swift
+++ iOS/App/BluetoothChat/Scenes/Join/TextFieldTableViewCell.swift
@@ -8,17 +8,10 @@
 
 import UIKit
 
-class TextFieldTableViewCell: UITableViewCell {
+class TextFieldTableViewCell: UITableViewCell, UITextFieldDelegate {
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-    
 }
