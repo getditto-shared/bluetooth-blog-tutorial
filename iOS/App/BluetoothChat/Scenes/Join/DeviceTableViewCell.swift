931ebb5cb4205166abbadbb808a7e9c5484372d4
Tim Oliver <me@timoliver.com.au>
2020-05-31 00:55:50 +0800

    Added cell configuration for join table view

--- iOS/App/BluetoothChat/Scenes/Join/DeviceTableViewCell.swift
+++ iOS/App/BluetoothChat/Scenes/Join/DeviceTableViewCell.swift
@@ -12,11 +12,17 @@ class DeviceTableViewCell: UITableViewCell {
 
     // Configure for when a specific device that was found
     public func configureForDevice(named device: String) {
-
+        textLabel?.alpha = 1.0
+        selectionStyle = .blue
+        accessoryType = .disclosureIndicator
+        textLabel?.text = device
     }
 
     // Configure for a default placeholder state when no devices are found
     public func configureForNoDevicesFound() {
-
+        textLabel?.alpha = 0.5
+        selectionStyle = .none
+        accessoryType = .none
+        textLabel?.text = "No devices found."
     }
 }
