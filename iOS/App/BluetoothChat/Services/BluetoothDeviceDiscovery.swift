e14bda77ce358396bc8d0e3e51cc64960322d62f
Tim Oliver <me@timoliver.com.au>
2020-05-31 12:38:14 +0800

    Added device discovery class

---
+++ iOS/App/BluetoothChat/Services/BluetoothDeviceDiscovery.swift
@@ -0,0 +1,14 @@
+//
+//  BluetoothDeviceDiscovery.swift
+//  BluetoothChat
+//
+//  Created by Tim Oliver on 31/5/20.
+//  Copyright © 2020 DittoLive Incorporated. All rights reserved.
+//
+
+import Foundation
+
+/// A class that both scans and advertisess
+class BluetoothDeviceDiscovery: NSObject {
+
+}
