743adb9205773e90bd234fed2f6c3243cd049a0c
Tim Oliver <me@timoliver.com.au>
2020-05-31 22:19:23 +0800

    Added display logic to join view controller

--- iOS/App/BluetoothChat/Scenes/Join/Cells/TextFieldTableViewCell.swift
+++ iOS/App/BluetoothChat/Scenes/Join/Cells/TextFieldTableViewCell.swift
@@ -11,11 +11,14 @@ import UIKit
 class TextFieldTableViewCell: UITableViewCell, UITextFieldDelegate {
 
     @IBOutlet weak var textField: UITextField!
+    public var textFieldChangedHandler: ((String) -> Void)?
 
     // MARK: Text Field Delegate
 
     func textFieldDidEndEditing(_ textField: UITextField) {
-
+        var deviceName = textField.text
+        if deviceName?.count ?? 0 == 0 { deviceName = textField.placeholder ?? "Ditto" }
+        textFieldChangedHandler?(deviceName!)
     }
 
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
--- iOS/App/BluetoothChat/Scenes/Join/JoinViewController.swift
+++ iOS/App/BluetoothChat/Scenes/Join/JoinViewController.swift
@@ -20,7 +20,12 @@ class JoinViewController: UITableViewController {
     static let nameCellIdentifier = "NameCell"
 
     // The name of this device
-    private var deviceName = UIDevice.current.name
+    private var deviceName = UIDevice.current.name {
+        didSet { deviceDiscovery.deviceName = deviceName }
+    }
+
+    // The Bluetooth service manager for advertising and scanning
+    private var deviceDiscovery: BluetoothDeviceDiscovery!
 
     // MARK: - Class Creation -
 
@@ -37,6 +42,13 @@ class JoinViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        // Set up and run the device discovery service
+        deviceDiscovery = BluetoothDeviceDiscovery(deviceName: deviceName)
+        deviceDiscovery.devicesListUpdatedHandler = { [weak self] in
+            guard let tableView = self?.tableView else { return }
+            tableView.reloadSections([Sections.availableDevices], with: .automatic)
+        }
 
         // Register the cells we plan to use
         tableView.register(DeviceTableViewCell.self,
@@ -63,26 +75,38 @@ class JoinViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if section == Sections.name { return 1 }
-        return 1
+        return max(deviceDiscovery.devices.count, 1)
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 
+        // For the name section, dequeue a text field cell and configure it
         if indexPath.section == Sections.name {
             let cell = tableView.dequeueReusableCell(withIdentifier: JoinViewController.nameCellIdentifier,
                                                      for: indexPath)
             if let nameCell = cell as? TextFieldTableViewCell {
                 nameCell.textField.text = deviceName
-                // Configure callbacks 
+                nameCell.textFieldChangedHandler = { [weak self] name in
+                    self?.deviceName = name
+                }
             }
 
             return cell
         }
 
+        // For the devices cells, dequeue one of the device cells and configure
         let cell = tableView.dequeueReusableCell(withIdentifier: JoinViewController.deviceCellIdentifier,
                                                  for: indexPath)
         if let deviceCell = cell as? DeviceTableViewCell {
-            deviceCell.configureForNoDevicesFound()
+
+            // If we have a list of devices, configure each cell with its name
+            if deviceDiscovery.devices.count > 0 {
+                let device = deviceDiscovery.devices[indexPath.row]
+                deviceCell.configureForDevice(named: device.name)
+            } else {
+                // If no devices found, show "no devices"
+                deviceCell.configureForNoDevicesFound()
+            }
         }
 
         return cell
